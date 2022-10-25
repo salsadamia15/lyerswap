c6af2a31bcf2ecd9ff7e538541a8d83b8b5c8564
babgev <babgev@gmail.com>
2022-10-25 23:24:54 +0400

    Replace plausible with vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import { Web3ReactProvider } from '@web3-react/core'
 import { Web3Provider } from '@ethersproject/providers'
 import { useRouter } from "next/router";
 import { IntercomProvider } from 'react-use-intercom';
+import { Analytics } from '@vercel/analytics/react';
 
 const getLibrary = () => {
   const provider = window.web3.currentProvider
@@ -18,6 +19,7 @@ function App({ Component, pageProps }) {
     <Web3ReactProvider getLibrary={getLibrary}>
       <IntercomProvider appId={INTERCOM_APP_ID}>
         <Component key={router.asPath} {...pageProps} />
+        <Analytics />
       </IntercomProvider>
     </Web3ReactProvider>)
 }
